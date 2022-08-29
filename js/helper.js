c0258a502e9d8b599a469e000a203b1c76f98f14
Álvaro Fernández Cascajosa <afernandez@MacBook-Pro-16-de-Alvaro.local>
2022-08-30 00:56:09 +0200

    Initial commit

---
+++ js/helper.js
@@ -0,0 +1,32 @@
+// Insert handwritten tags
+const elementsWithTags = document.querySelectorAll(".handwritten-tag");
+
+elementsWithTags.forEach((element) => {
+  const tagName = element.tagName.toLowerCase();
+  const customTag = element.dataset.handwrittenTag ?? tagName;
+
+  const openTag = document.createElement("span");
+  openTag.classList.add("qwitcher-grypen");
+  openTag.innerHTML = `&#60;${customTag}&#62;`;
+
+  const closeTag = document.createElement("span");
+  closeTag.classList.add("qwitcher-grypen");
+  closeTag.innerHTML = `&#60;/${customTag}&#62;`;
+
+  element.insertAdjacentElement("afterbegin", openTag);
+  element.insertAdjacentElement("beforeend", closeTag);
+});
+
+function darkMode() {
+    const body = document.body;
+    const isDarkMode = body.classList.toggle("dark-mode");
+    const themeColor = isDarkMode ? "#34495f" : "#ffffff";
+    const darkModeIcon = document.getElementById("dark-mode-icon");
+    const bulmaLogo = document.getElementById("made-with-bulma");
+  
+    darkModeIcon.classList.toggle("fa-sun", isDarkMode);
+    darkModeIcon.classList.toggle("fa-moon", !isDarkMode);
+    document.querySelector("meta[name='theme-color']").content = themeColor;
+    bulmaLogo.src = isDarkMode ? "img/made-with-bulma-white.png" : "img/made-with-bulma.png";
+  }
+  
