f79bbce275556717ec59148f799d71b7478b9db0
Alvfercas <afernandezcascajosa@gmail.com>
2024-10-31 10:28:02 +0100

    feat: now description shares with LD and body. Added Sevimetria link

--- js/helper.js
+++ js/helper.js
@@ -1,45 +1,72 @@
 // Insert handwritten tags
-const elementsWithTags = document.querySelectorAll(".handwritten-tag");
+const elementsWithTags = document.querySelectorAll('.handwritten-tag')
 
 elementsWithTags.forEach((element) => {
-  const tagName = element.tagName.toLowerCase();
-  const customTag = element.dataset.handwrittenTag ?? tagName;
+  const tagName = element.tagName.toLowerCase()
+  const customTag = element.dataset.handwrittenTag ?? tagName
 
-  const openTag = document.createElement("span");
-  openTag.classList.add("qwitcher-grypen");
-  openTag.innerHTML = `&#60;${customTag}&#62;`;
+  const openTag = document.createElement('span')
+  openTag.classList.add('qwitcher-grypen')
+  openTag.innerHTML = `&#60;${customTag}&#62;`
 
-  const closeTag = document.createElement("span");
-  closeTag.classList.add("qwitcher-grypen");
-  closeTag.innerHTML = `&#60;/${customTag}&#62;`;
+  const closeTag = document.createElement('span')
+  closeTag.classList.add('qwitcher-grypen')
+  closeTag.innerHTML = `&#60;/${customTag}&#62;`
 
-  element.insertAdjacentElement("afterbegin", openTag);
-  element.insertAdjacentElement("beforeend", closeTag);
-});
+  element.insertAdjacentElement('afterbegin', openTag)
+  element.insertAdjacentElement('beforeend', closeTag)
+})
 
 // Match system aspect preferences
-if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
-  darkMode();
+if (
+  window.matchMedia &&
+  window.matchMedia('(prefers-color-scheme: dark)').matches
+) {
+  darkMode()
 }
 
 function darkMode() {
-    const body = document.body;
-    const isDarkMode = body.classList.toggle("dark-mode");
-    const themeColor = isDarkMode ? "#34495f" : "#ffffff";
-    const darkModeIcon = document.getElementById("dark-mode-icon");
-  
-    darkModeIcon.classList.toggle("fa-sun", isDarkMode);
-    darkModeIcon.classList.toggle("fa-moon", !isDarkMode);
-    document.querySelector("meta[name='theme-color']").content = themeColor;
+  const body = document.body
+  const isDarkMode = body.classList.toggle('dark-mode')
+  const themeColor = isDarkMode ? '#34495f' : '#ffffff'
+  const darkModeIcon = document.getElementById('dark-mode-icon')
+
+  darkModeIcon.classList.toggle('fa-sun', isDarkMode)
+  darkModeIcon.classList.toggle('fa-moon', !isDarkMode)
+  document.querySelector("meta[name='theme-color']").content = themeColor
 }
 
 const dateDifferenceInYears = (dateInitial, dateFinal) => {
-  const millisecondsPerYear = 1000 * 60 * 60 * 24 * 365;
-  return (dateFinal - dateInitial) / millisecondsPerYear;
+  const millisecondsPerYear = 1000 * 60 * 60 * 24 * 365
+  return (dateFinal - dateInitial) / millisecondsPerYear
 }
 
-const dateInitial = new Date('2015-03-01');
-const dateFinal = new Date();
-const yearsDifference = dateDifferenceInYears(dateInitial, dateFinal);
+document.addEventListener('DOMContentLoaded', () => {
+  const dateInitial = new Date('2015-03-01')
+  const dateFinal = new Date()
+  const yearsDifference = dateDifferenceInYears(dateInitial, dateFinal)
 
-document.getElementById('yearsDifference').textContent = yearsDifference.toFixed() + " years";
+  const sharedDescription = document.getElementById('shared-description')
+
+  const updatedDescriptionWithTags = sharedDescription.content.replace(
+    '{{years}}',
+    '<strong>' + yearsDifference.toFixed() + ' years</strong>'
+  )
+  const updatedDescription = sharedDescription.content.replace(
+    '{{years}}',
+    yearsDifference.toFixed() + ' years'
+  )
+
+  console.log(updatedDescription)
+
+  const ldJsonScript = document.querySelector(
+    'script[type="application/ld+json"]'
+  )
+  if (ldJsonScript) {
+    const ldJson = JSON.parse(ldJsonScript.innerHTML)
+    ldJson['@graph'][0].description = updatedDescription
+    ldJsonScript.innerHTML = JSON.stringify(ldJson, null, 2)
+  }
+
+  document.getElementById('description').innerHTML = updatedDescriptionWithTags
+})
